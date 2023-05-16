efd202fcfb5fb1369ad5d85037724681deab9035
Dennis E. Mungai <2356871+Brainiarc7@users.noreply.github.com>
2023-05-16 19:15:44 +0300

    Update 50-decklink.sh

--- scripts.d/50-decklink.sh
+++ scripts.d/50-decklink.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# https://notabug.org/RiCON/decklink-headers.git
-DECKLINK_REPO="https://github.com/nanake/decklink-headers.git"
-DECKLINK_COMMIT="SDK/12.4"
+SCRIPT_REPO="https://github.com/Brainiarc7/blackmagic-sdk.git"
+SCRIPT_COMMIT="5a93d2f3f3ff87bf690e689e165aaba8ee4812b5"
 
 ffbuild_enabled() {
-    [[ $VARIANT == *nonfree* ]] || return -1
+    [[ $VARIANT == nonfree* ]] || return -1
+    [[ $TARGET != linux* ]] && return -1
+    [[ $TARGET == linuxarm64 ]] && return -1    
     return 0
 }
 
+
 ffbuild_dockerbuild() {
-    git-mini-clone "$DECKLINK_REPO" "$DECKLINK_COMMIT" decklink
+    git-mini-clone "$SCRIPT_REPO" "$SCRIPT_COMMIT" decklink
     cd decklink
 
-    make PREFIX="$FFBUILD_PREFIX" install
+    mkdir -p "$FFBUILD_PREFIX"/include
+    mv Linux/include/* "$FFBUILD_PREFIX"/include/
 }
 
 ffbuild_configure() {
