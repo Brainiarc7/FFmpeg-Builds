6c3f63da6f1c959b049bd482836c645d36e3d08f
Dennis E. Mungai <2356871+Brainiarc7@users.noreply.github.com>
2023-06-02 17:20:16 +0300

    Create 25-librtmp.sh

---
+++ scripts.d/25-librtmp.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/ossrs/librtmp.git"
+SCRIPT_COMMIT="f1b83c10d8beb43fcc70a6e88cf4325499f25857"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$SCRIPT_REPO" "$SCRIPT_COMMIT" librtmp
+    cd librtmp
+
+    local myconf=(
+        CFLAGS=-I$FFBUILD_PREFIX/include
+        LDFLAGS=-L$FFBUILD_PREFIX/lib
+        SHARED= SO_INST= CRYPTO=OPENSSL
+        --prefix="$FFBUILD_PREFIX"
+    )
+
+    if [[ $TARGET == win64 ]]; then
+        myconf+=(
+            make CROSS_COMPILE="x86_64-w64-mingw64-" INC=-I$FFBUILD_CROSS_PREFIX/include
+            mingw64
+        )
+    elif [[ $TARGET == win32 ]]; then
+        myconf+=(
+            make CROSS_COMPILE="i686-w64-mingw32-" INC=-I$FFBUILD_CROSS_PREFIX/include
+            mingw
+        )
+    elif [[ $TARGET == linux64 ]]; then
+        myconf+=(
+            make CROSS_COMPILE="x86_64-linux-gnu" INC=-I$FFBUILD_CROSS_PREFIX/include
+            linux-x86_64
+        )
+    elif [[ $TARGET == linuxarm64 ]]; then
+        myconf+=(
+            make CROSS_COMPILE="aarch64-linux-gnu-" INC=-I$FFBUILD_CROSS_PREFIX/include
+            linux-aarch64
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    export CFLAGS="$CFLAGS -fno-strict-aliasing"
+    export CXXFLAGS="$CXXFLAGS -fno-strict-aliasing"
+
+    # librtmp build system prepends the cross prefix itself
+    export CC="${CC/${FFBUILD_CROSS_PREFIX}/}"
+    export CXX="${CXX/${FFBUILD_CROSS_PREFIX}/}"
+    export AR="${AR/${FFBUILD_CROSS_PREFIX}/}"
+    export RANLIB="${RANLIB/${FFBUILD_CROSS_PREFIX}/}"
+
+
+    make -j$(nproc)
+    make install
+}
+
+ffbuild_configure() {
+    [[ $TARGET == win* ]] && return 0
+    echo ----enable-librtmp
+}
