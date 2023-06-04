63821ae341fa7a7f96e4068744d1c34387b2b101
Dennis E. Mungai <2356871+Brainiarc7@users.noreply.github.com>
2023-06-04 12:25:30 +0300

    Switch to gnutls for this branch
    Switch to gnutls for this branch

---
+++ scripts.d/25-gnutls.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/gnutls/gnutls.git"
+SCRIPT_COMMIT="gnutls_3_7_x"
+SCRIPT_TAGFILTER="gnutls_3_7_*"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$SCRIPT_REPO" "$SCRIPT_COMMIT" openssl
+    cd gnutls
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --enable-maintainer-mode
+        --disable-shared
+        --enable-static
+        --with-pic
+        --with-included-libtasn1 
+        --with-included-unistring 
+        --without-p11-kit 
+        --disable-doc 
+        --disable-c
+    )
+
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    ./configure "${myconf[@]}"
+    make -j$(nproc)
+    make install
+}
+
+ffbuild_configure() {
+    echo --enable-gnutls
+}
+
+ffbuild_unconfigure() {
+    echo --disable-gnutls
+}
