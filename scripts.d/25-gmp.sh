9e3cd49471d6fa41a2275154eb5a30de79c34ac9
Dennis E. Mungai <2356871+Brainiarc7@users.noreply.github.com>
2023-06-05 03:39:42 +0300

    Update 25-gmp.sh

--- scripts.d/25-gmp.sh
+++ scripts.d/25-gmp.sh
@@ -15,7 +15,9 @@ ffbuild_dockerbuild() {
 
     local myconf=(
         --prefix="$FFBUILD_PREFIX"
-        --enable-shared
+        --enable-maintainer-mode
+        --disable-shared
+        --enable-static
         --with-pic
     )
 
@@ -31,4 +33,12 @@ ffbuild_dockerbuild() {
     ./configure "${myconf[@]}"
     make -j$(nproc)
     make install
+}
+
+ffbuild_configure() {
+    echo --enable-gmp
+}
+
+ffbuild_unconfigure() {
+    echo --disable-gmp
 }
