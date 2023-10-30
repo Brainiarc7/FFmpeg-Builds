97fe44176b05b3930af8661a0bf740c64a0ab36d
Dennis E. Mungai <2356871+Brainiarc7@users.noreply.github.com>
2023-10-30 13:04:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -50,9 +50,9 @@ rm -f "$TESTFILE"
 rm -rf ffbuild
 mkdir ffbuild
 
-FFMPEG_REPO="${FFMPEG_REPO:-https://github.com/Brainiarc7/FFmpeg.git}"
+FFMPEG_REPO="${FFMPEG_REPO:-https://github.com/LTNGlobal-opensource/FFmpeg-ltn.git}"
 FFMPEG_REPO="${FFMPEG_REPO_OVERRIDE:-$FFMPEG_REPO}"
-GIT_BRANCH="${GIT_BRANCH:-master}"
+GIT_BRANCH="${GIT_BRANCH:-12m_x264}"
 GIT_BRANCH="${GIT_BRANCH_OVERRIDE:-$GIT_BRANCH}"
 
 BUILD_SCRIPT="$(mktemp)"
