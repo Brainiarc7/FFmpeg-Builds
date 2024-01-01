7667259a9cae50a5d4e481d693d8fec03460338a
Dennis E. Mungai <2356871+Brainiarc7@users.noreply.github.com>
2024-01-01 22:51:13 +0300

    Switch to my fork of Intel's ffmpeg staging

--- build.sh
+++ build.sh
@@ -51,7 +51,7 @@ unset TESTFILE
 rm -rf ffbuild
 mkdir ffbuild
 
-FFMPEG_REPO="${FFMPEG_REPO:-https://github.com/intel-media-ci/ffmpeg.git}"
+FFMPEG_REPO="${FFMPEG_REPO:-https://github.com/Brainiarc7/intel-ffmpeg-staging.git}"
 FFMPEG_REPO="${FFMPEG_REPO_OVERRIDE:-$FFMPEG_REPO}"
 GIT_BRANCH="${GIT_BRANCH:-master}"
 GIT_BRANCH="${GIT_BRANCH_OVERRIDE:-master}"
