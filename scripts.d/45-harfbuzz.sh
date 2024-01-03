622f20af22b915a71a17e3b37fa24b949fadaf64
Dennis E. Mungai <2356871+Brainiarc7@users.noreply.github.com>
2024-01-03 21:13:42 +0300

    Build harfbuzz as a dependency but don't enable it in FFmpeg
    Build harfbuzz as a dependency but don't enable it in FFmpeg

---
+++ scripts.d/45-harfbuzz.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/harfbuzz/harfbuzz.git"
+SCRIPT_COMMIT="f3efa6f6e54740214739ba8b00e777111e781882"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --disable-shared
+        --enable-static
+        --with-pic
+    )
+
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    export LIBS="-lpthread"
+
+    ./autogen.sh "${myconf[@]}"
+    make -j$(nproc)
+    make install
+}
